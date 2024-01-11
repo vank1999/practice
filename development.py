3fbb7eea26ee3bfcdb60ec516754f4f9a065f930
anil <anilkumarvallepu1@gmail.com>
2024-01-11 19:03:52 +0530

    fix is done

--- development.py
+++ development.py
@@ -1,3 +1,3 @@
 print("this is dev branch code")
 print("issue is fixed")
-print("checking the difference between master and dev branch")
+#print("checking the diqfference between master and dev branch")
